41d4750a5263dd4e9530c136b7f4f6074d8b022a
Oleg <verdrehung@gmail.com>
2011-09-12 18:20:58 +0700

    Initial commit

---
+++ lib/AnyEvent/Whois/Raw.pm
@@ -0,0 +1,27 @@
+package AnyEvent::Whois::Raw;
+
+use base 'Exporter';
+use Net::Whois::Raw::Common;
+use AnyEvent;
+use AnyEvent::HTTP;
+use strict;
+
+our @EXPORT_OK = qw(whois whois_get);
+
+sub whois {
+
+}
+
+sub whois_get {
+
+}
+
+sub whois_query {
+
+}
+
+sub www_whois_query {
+
+}
+
+1;
