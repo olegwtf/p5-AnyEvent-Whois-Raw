13a9c513ae7f3168400d8be3dcbe6a49e0ec4f16
Oleg <verdrehung@gmail.com>
2011-09-13 11:30:19 +0700

    will try to rewrite as little as possible

--- lib/AnyEvent/Whois/Raw.pm
+++ lib/AnyEvent/Whois/Raw.pm
@@ -1,27 +1,58 @@
 package AnyEvent::Whois::Raw;
 
 use base 'Exporter';
-use Net::Whois::Raw::Common;
+use Net::Whois::Raw ();
 use AnyEvent;
+use AnyEvent::Socket;
 use AnyEvent::HTTP;
 use strict;
+no warnings 'redefine';
 
-our @EXPORT_OK = qw(whois whois_get);
+our @EXPORT_OK = qw(whois get_whois);
+our $stash;
 
 sub whois {
-
+	my $cb = pop;
+	local $stash = {};
+	
+	my ($res_text, $res_srv);
+	while (1) {
+		eval {
+			($res_text, $res_srv) = Net::Whois::Raw::whois(@_);
+		} and last;
+	}
+	
+	$cb->($res_text, $res_srv);
 }
 
 sub whois_get {
-
+	my $cb = pop;
+	local $stash = {};
+	
+	my ($res_text, $res_srv);
+	while (1) {
+		eval {
+			($res_text, $res_srv) = Net::Whois::Raw::get_whois(@_);
+		} and last;
+	}
+	
+	$cb->($res_text, $res_srv);
 }
 
-sub whois_query {
-
+sub Net::Whois::Raw::whois_query {
+	
 }
 
-sub www_whois_query {
+sub whois_query_ae {
+	
+}
 
+sub Net::Whois::Raw::www_whois_query {
+	
+}
+
+sub www_whois_query_ae {
+	
 }
 
 1;
